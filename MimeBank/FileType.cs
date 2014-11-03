ae5a9667eb1569c4a122371188f52d021e7e4b01
Sedat Kapanoglu <ssg@users.noreply.github.com>
2014-11-03 13:40:47 +0200

    Remove explicit numeric values from enum

--- MimeBank/FileType.cs
+++ MimeBank/FileType.cs
@@ -6,16 +6,12 @@ using System.Threading.Tasks;
 
 namespace MimeBank
 {
-    /// <summary>
-    /// Just an enum to categorize the file types.
-    /// </summary>
     public enum FileType
     {
-        Image = 1,          
-        Video = 2,
-        Audio = 3,
-        Swf = 4,
-        Other = 255
+        Unknown = 0,
+        Image,
+        Video,
+        Audio,
+        Swf,
     }
-
 }
